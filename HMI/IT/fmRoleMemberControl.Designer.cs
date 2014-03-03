96a992b8a6b359d73cff2e588608680a362daa2d
foxliu36 <fox.liu36@gmail.com>
2014-03-03 17:20:12 +0800

    v2

--- HMI/IT/fmRoleMemberControl.Designer.cs
+++ HMI/IT/fmRoleMemberControl.Designer.cs
@@ -32,8 +32,19 @@
             this.lbModule = new System.Windows.Forms.ListBox();
             this.lbRoleID = new System.Windows.Forms.ListBox();
             this.btnProfile = new System.Windows.Forms.Button();
-            this.userControl11 = new HMI.PublicUnit.UserControl1();
+            this.dgvAuth = new System.Windows.Forms.DataGridView();
+            this.cbAuthAdd = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAuthAdd = new System.Windows.Forms.Button();
+            this.txtDetail1 = new System.Windows.Forms.TextBox();
+            this.btnDetailCheck = new System.Windows.Forms.Button();
+            this.txtDetail2 = new System.Windows.Forms.TextBox();
+            this.lblDetail1 = new System.Windows.Forms.Label();
+            this.lblDetail2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAuth)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // dataGridView1
@@ -42,7 +53,8 @@
             this.dataGridView1.Location = new System.Drawing.Point(12, 154);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(297, 133);
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(297, 81);
             this.dataGridView1.TabIndex = 0;
             // 
             // lbModule
@@ -67,7 +79,7 @@
             // 
             // btnProfile
             // 
-            this.btnProfile.Location = new System.Drawing.Point(326, 212);
+            this.btnProfile.Location = new System.Drawing.Point(327, 154);
             this.btnProfile.Name = "btnProfile";
             this.btnProfile.Size = new System.Drawing.Size(75, 23);
             this.btnProfile.TabIndex = 5;
@@ -75,28 +87,127 @@
             this.btnProfile.UseVisualStyleBackColor = true;
             this.btnProfile.Click += new System.EventHandler(this.btnProfile_Click);
             // 
-            // userControl11
+            // dgvAuth
             // 
-            this.userControl11.Location = new System.Drawing.Point(338, 12);
-            this.userControl11.Margin = new System.Windows.Forms.Padding(2);
-            this.userControl11.Name = "userControl11";
-            this.userControl11.Size = new System.Drawing.Size(367, 46);
-            this.userControl11.TabIndex = 1;
+            this.dgvAuth.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAuth.Location = new System.Drawing.Point(408, 12);
+            this.dgvAuth.Name = "dgvAuth";
+            this.dgvAuth.RowTemplate.Height = 24;
+            this.dgvAuth.Size = new System.Drawing.Size(550, 474);
+            this.dgvAuth.TabIndex = 7;
+            // 
+            // cbAuthAdd
+            // 
+            this.cbAuthAdd.FormattingEnabled = true;
+            this.cbAuthAdd.Items.AddRange(new object[] {
+            "人員",
+            "部門",
+            "職級",
+            "職務和部門"});
+            this.cbAuthAdd.Location = new System.Drawing.Point(79, 36);
+            this.cbAuthAdd.Name = "cbAuthAdd";
+            this.cbAuthAdd.Size = new System.Drawing.Size(121, 20);
+            this.cbAuthAdd.TabIndex = 8;
+            this.cbAuthAdd.SelectedIndexChanged += new System.EventHandler(this.cbAuthAdd_SelectedIndexChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblDetail2);
+            this.groupBox1.Controls.Add(this.lblDetail1);
+            this.groupBox1.Controls.Add(this.txtDetail2);
+            this.groupBox1.Controls.Add(this.btnDetailCheck);
+            this.groupBox1.Controls.Add(this.txtDetail1);
+            this.groupBox1.Controls.Add(this.btnAuthAdd);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.cbAuthAdd);
+            this.groupBox1.Location = new System.Drawing.Point(12, 241);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(246, 339);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "新增權限";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 12);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "範圍種類:";
+            // 
+            // btnAuthAdd
+            // 
+            this.btnAuthAdd.Location = new System.Drawing.Point(134, 310);
+            this.btnAuthAdd.Name = "btnAuthAdd";
+            this.btnAuthAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAuthAdd.TabIndex = 10;
+            this.btnAuthAdd.Text = "新增範圍";
+            this.btnAuthAdd.UseVisualStyleBackColor = true;
+            this.btnAuthAdd.Click += new System.EventHandler(this.btnAuthAdd_Click);
+            // 
+            // txtDetail1
+            // 
+            this.txtDetail1.Location = new System.Drawing.Point(79, 80);
+            this.txtDetail1.Name = "txtDetail1";
+            this.txtDetail1.Size = new System.Drawing.Size(118, 22);
+            this.txtDetail1.TabIndex = 11;
+            // 
+            // btnDetailCheck
+            // 
+            this.btnDetailCheck.Location = new System.Drawing.Point(19, 310);
+            this.btnDetailCheck.Name = "btnDetailCheck";
+            this.btnDetailCheck.Size = new System.Drawing.Size(75, 23);
+            this.btnDetailCheck.TabIndex = 12;
+            this.btnDetailCheck.Text = "查詢";
+            this.btnDetailCheck.UseVisualStyleBackColor = true;
+            this.btnDetailCheck.Click += new System.EventHandler(this.btnDetailCheck_Click);
+            // 
+            // txtDetail2
+            // 
+            this.txtDetail2.Enabled = false;
+            this.txtDetail2.Location = new System.Drawing.Point(79, 159);
+            this.txtDetail2.Name = "txtDetail2";
+            this.txtDetail2.Size = new System.Drawing.Size(118, 22);
+            this.txtDetail2.TabIndex = 13;
+            // 
+            // lblDetail1
+            // 
+            this.lblDetail1.AutoSize = true;
+            this.lblDetail1.Location = new System.Drawing.Point(11, 83);
+            this.lblDetail1.Name = "lblDetail1";
+            this.lblDetail1.Size = new System.Drawing.Size(62, 12);
+            this.lblDetail1.TabIndex = 14;
+            this.lblDetail1.Text = "範圍細節1:";
+            // 
+            // lblDetail2
+            // 
+            this.lblDetail2.AutoSize = true;
+            this.lblDetail2.Enabled = false;
+            this.lblDetail2.Location = new System.Drawing.Point(11, 162);
+            this.lblDetail2.Name = "lblDetail2";
+            this.lblDetail2.Size = new System.Drawing.Size(62, 12);
+            this.lblDetail2.TabIndex = 15;
+            this.lblDetail2.Text = "範圍細節2:";
             // 
             // fmRoleMemberControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(970, 592);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dgvAuth);
             this.Controls.Add(this.btnProfile);
             this.Controls.Add(this.lbRoleID);
             this.Controls.Add(this.lbModule);
-            this.Controls.Add(this.userControl11);
             this.Controls.Add(this.dataGridView1);
             this.Name = "fmRoleMemberControl";
             this.Text = "腳色成員管理";
             this.Load += new System.EventHandler(this.fmRoleMemberControl_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAuth)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -104,9 +215,18 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
-        private PublicUnit.UserControl1 userControl11;
         private System.Windows.Forms.ListBox lbModule;
         private System.Windows.Forms.ListBox lbRoleID;
         private System.Windows.Forms.Button btnProfile;
+        private System.Windows.Forms.DataGridView dgvAuth;
+        private System.Windows.Forms.ComboBox cbAuthAdd;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAuthAdd;
+        private System.Windows.Forms.Button btnDetailCheck;
+        private System.Windows.Forms.TextBox txtDetail1;
+        private System.Windows.Forms.Label lblDetail2;
+        private System.Windows.Forms.Label lblDetail1;
+        private System.Windows.Forms.TextBox txtDetail2;
     }
 }
--- HMI/IT/fmRoleMemberControl.cs
+++ HMI/IT/fmRoleMemberControl.cs
@@ -16,6 +16,8 @@ namespace HMI.IT
     public partial class fmRoleMemberControl : Form
     {
 
+        //紀錄各種腳色範圍
+        List<Auth> la = new List<Auth>();
 
         public fmRoleMemberControl()
         {
@@ -89,27 +91,74 @@ namespace HMI.IT
 
         /// <summary>
         /// 解析
+        // <userId> = [TB_EB_USER].USER_GUID    員工主檔
+        // <jobTitleId> = [TB_EB_JOB_TITLE].[TITLE_ID]  職級主檔
+        // <jobFunctionId> = [TB_EB_JOB_FUNC].[FUNC_ID]  職務主檔
+        // <groupId> = [TB_EB_GROUP].[GROUP_ID] 部門主檔
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void btnProfile_Click(object sender, EventArgs e)
         {
+            la.Clear();
+
             string xml = dataGridView1.SelectedRows[0].Cells["USER_SET"].Value.ToString();
 
             XDocument doc = XDocument.Parse(xml);
 
-            IEnumerable<XElement> targetNodes = from q in doc.Descendants("userId")
-                                                select q;
+            var elementtype = from q in doc.Descendants("Element")
+                              group q by q.Attribute("type").Value into g
+                              select g.Key;
+                              
 
+            //取得userid
+            var usernode = from q in doc.Descendants("userId")
+                           select new Auth
+                           {
+                               範圍種類 = "人員",
+                               範圍1 = getUser(q.Value).NAME,
+                               key1 = q.Value
+                           };
 
-            var targetNodess = from q in doc.Descendants("Element")
-                               where q.Attribute("type").Value == "jobFunctionOfGroup"
-                               select new {
-                                   jobFunctionId = q.Descendants("jobFunctionId"),
-                                   groupId = q.Descendants("groupId")
+            foreach (var item in usernode)
+            {
+                la.Add(item);
+            }
+            
+            //取得jobTitle
+            var jobtitlenode = from q in doc.Descendants("jobTitleId")
+                               select new Auth
+                               {
+                                   範圍種類 = "職級",
+                                   範圍1 = getJobTitle(q.Value).TITLE_NAME,
+                                   key1 = q.Value
                                };
+            foreach (var item in jobtitlenode)
+            {
+                la.Add(item);
+            }
 
+            var jobfuncgroupNodes = from q in doc.Descendants("Element")
+                               where q.Attribute("type").Value == "jobFunctionOfGroup"
+                               select new Auth
+                               {
+                                   範圍種類 = "職務和部門",
+                                   範圍1 = getJobFunc(q.Descendants("jobFunctionId").First().Value).FUNC_NAME,
+                                   key1 = q.Descendants("jobFunctionId").First().Value,
+                                   範圍2 = getGroup(q.Descendants("groupId").First().Value).GROUP_NAME,
+                                   key2 = q.Descendants("groupId").First().Value
+                               };
+            foreach (var item in jobfuncgroupNodes)
+            {
+                la.Add(item);
+            }
 
+            dgvAuth.DataSource = (from q in la
+                                 select new {
+                                     權限名稱 = q.範圍種類,
+                                     範圍1 = q.範圍1,
+                                     範圍2 = q.範圍2,
+                                 }).ToList();
             #region 失敗的方法
             //XmlProfile proxml = new XmlProfile(xml, "");
             //紀錄上一個節點名稱
@@ -119,13 +168,6 @@ namespace HMI.IT
             //List<string> Groupid = new List<string>();
             //List<string> JobTitleid = new List<string>();
             //List<string> JobTitleOfGroup = new List<string>();
-
-            // <userId> = [TB_EB_USER].USER_GUID    員工主檔
-            // <jobTitleId> = [TB_EB_JOB_TITLE].[TITLE_ID]  職級主檔
-            //下面兩個要再一起
-            // <jobFunctionId> = [TB_EB_JOB_FUNC].[FUNC_ID]  職務主檔
-            // <groupId> = [TB_EB_GROUP].[GROUP_ID] 部門主檔
-
 
             //while (proxml.GetXmlReader().Read())
             //{
@@ -177,9 +219,148 @@ namespace HMI.IT
             
         }
 
-        private void bra() 
+        /// <summary>
+        /// 選擇新增的範圍總類
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void cbAuthAdd_SelectedIndexChanged(object sender, EventArgs e)
         {
-        
+            switch (cbAuthAdd.SelectedItem.ToString())
+            {
+                case "人員":
+                    lblDetail1.Text = "員編";
+                    lblDetail2.Enabled = false;
+                    txtDetail2.Enabled = false;
+                    break;
+                case "部門":
+                    lblDetail1.Text = "部門";
+                    lblDetail2.Enabled = false;
+                    txtDetail2.Enabled = false;
+                    break;
+                case "職級":
+                    lblDetail1.Text = "職級";
+                    lblDetail2.Enabled = false;
+                    txtDetail2.Enabled = false;
+                    break;
+                case "職務和部門":
+                    lblDetail1.Text = "職務和部門";
+                    lblDetail2.Enabled = true;
+                    txtDetail2.Enabled = true;
+                    break;
+                default:
+                    break;
+            }
         }
+
+        //查詢新增範圍資料
+        private void btnDetailCheck_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// 新增範圍
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnAuthAdd_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        //腳色範圍
+        private class Auth
+        {
+            private string type;
+            private string a1;
+            private string k1;
+            private string a2;
+            private string k2;
+
+            public string 範圍種類 {
+                get { return type; }
+                set { type = value; }
+            }
+
+            public string 範圍1
+            {
+                get { return a1; }
+                set { a1 = value; }
+            }
+
+            public string key1
+            {
+                get { return k1; }
+                set { k1 = value; }
+            }
+
+            public string 範圍2
+            {
+                get { return a2; }
+                set { a2 = value; }
+            }
+
+            public string key2
+            {
+                get { return k2; }
+                set { k2 = value; }
+            }
+        }
+
+        //職務配部門的範圍
+        private class JobFunctionOfGroupAuth 
+        {
+            public XElement jobfuncid { set; get; }
+            public XElement groupid { set; get; }
+        }
+
+        //找人
+        private TB_EB_USER getUser(string p_EmpGUID) 
+        {
+            using (var en = new UOFEntities())
+            {
+                TB_EB_USER user = en.TB_EB_USER.Where(p => p.USER_GUID == p_EmpGUID).First();
+                return user;
+            }
+        }
+        
+        //找職級 ex:專員
+        private TB_EB_JOB_TITLE getJobTitle(string p_TITLE_ID) 
+        {
+            using (var en = new UOFEntities())
+            {
+                TB_EB_JOB_TITLE title = en.TB_EB_JOB_TITLE.Where(p => p.TITLE_ID == p_TITLE_ID).First();
+                return title;
+            }
+        }
+
+        //找職務 ex:廠長
+        private TB_EB_JOB_FUNC getJobFunc(string p_FuncID) 
+        {
+            using (var en = new UOFEntities())
+            {
+                TB_EB_JOB_FUNC jfunc = en.TB_EB_JOB_FUNC.Where(p => p.FUNC_ID == p_FuncID).First();
+                return jfunc;
+            }
+        }
+
+        //找部門 ex:青年廠
+        private TB_EB_GROUP getGroup(string p_GroupID) 
+        {
+            using (var en = new UOFEntities())
+            {
+                TB_EB_GROUP group = en.TB_EB_GROUP.Where(p => p.GROUP_ID == p_GroupID).First();
+                return group;
+            }
+        }
+
+        
+
+        
+
+        
     }
 }
